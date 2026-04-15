c1a951c76dc8220e61435eeb8da9f3fd5f4bbde1
El Negroo <quirozstiven9831@gmail.com>
2026-04-14 20:39:51 -0500

    Proyecto integrador

---
+++ TalenteHM1/src/test/java/com/cesde/TalenteHM1/TalenteHm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.TalenteHM1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalenteHm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
